2eb777b5f7f2c3ad4e395e4bd469c09b7bc304a9
Apolinapolis <larra12@ya.ru>
2024-06-11 13:45:43 +0300

    fix structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Login } from './components/LoginPage';
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    // <Routes>
+        <Login />
+    // </Routes>
+  );
+}
+
+export default App;
